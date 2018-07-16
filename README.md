0734487919e604331835c6a4e7364f2777077575
GBTC440008UR <GBTC440008UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-16 13:30:32 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# Spring-Securty-17_18-" 
+"# Spring19-Security" 
---
+++ src/main/java/com/dulal/spring_security_19/HomeController.java
@@ -0,0 +1,23 @@
+package com.dulal.spring_security_19;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String loginPage(){
+        return "login";
+    }
+    @RequestMapping("/admin")
+    public  String admin(){
+        return "admin";
+    }
+
+}
